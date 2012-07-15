49c1d7ab956964282ab6f56597c0c139f8b5d760
James Halliday <mail@substack.net>
2012-07-14 19:48:00 -0700

    initial thing taken from bouncer

---
+++ index.js
@@ -0,0 +1,102 @@
+var pick = require('deck').pick;
+var isRegExp = require('is').is.regexp;
+
+var bouncy = require('bouncy');
+var merge = require('merge');
+
+module.exports = function (ports, opts) {
+    if (!opts) opts = {};
+    
+    var server = bouncy(opts, function (req, bounce) {
+        for (var i = 0; i < hooks.length; i++) {
+            if (hooks[i].cmp(req)) {
+                return hooks[i].cb(req, bounce);
+            }
+        }
+        
+        handler(ports, req, bounce);
+    });
+    
+    var hooks = [];
+    
+    server.mount = function (params, cb) {
+        if (typeof params === 'string' || isRegExp(params)) {
+            params = { path : params };
+        }
+        
+        var cmp = function (req) {
+            if (typeof params === 'function') {
+                return params(req);
+            }
+            
+            if (isRegExp(params.path)) {
+                return params.path.test(req.url);
+            }
+            
+            var u = req.url.slice(0, params.path.length);
+            var c = req.url.charAt(params.path.length);
+            return u === params.path && (c === '' || /[\/;?]/.test(c));
+        };
+        
+        hooks.push({ cmp : cmp, cb : cb });
+    };
+    
+    return server;
+};
+ 
+function handler (ports, req, bounce) {
+    function error (code, msg) {
+        var res = bounce.respond();
+        res.writeHead(code, { 'content-type' : 'text/plain' })
+        res.end(msg + '\r\n');
+    }
+    
+    var host = req.headers.host;
+    if (/^www\./.test(host)) {
+        var r = bounce.respond()
+        r.writeHead(302, {
+            location : 'http://' + host.replace(/^www\./, ''),
+            'content-type' : 'text/plain',
+        })
+        r.end();
+        return;
+    }
+    
+    var subdomain = (host || 'browserling.com')
+        .split('.').slice(0,-2).join('.')
+        || 'browserling'
+    ;
+    
+    req.on('error', function () { req.socket.destroy() });
+    req.socket.on('error', function () { req.socket.destroy() });
+    
+    var ps = ports.query('web.' + subdomain);
+    var mounts = ps.filter(function (p) {
+        if (typeof p.mount !== 'string') return false;
+        
+        var c = req.url.charAt(p.mount.length);
+        return p.mount === req.url.slice(0, p.mount.length) && (
+            /[?\/;]$/.test(p.mount) || c === '' || /[?\/;]/.test(c)
+        );
+    });
+    var webservers = ps.filter(function (p) { return !p.mount });
+    
+    var stream;
+    if (mounts.length) {
+        var m = pick(mounts);
+        stream = bounce(merge({
+            path : req.url.slice(m.mount.length).replace(/^\/*/, '/')
+        }, m))
+    }
+    else if (webservers.length) {
+        stream = bounce(pick(webservers))
+    }
+    else {
+        return error(404, 'no matching services are running');
+    }
+    
+    stream.on('error', function () {
+        stream.destroy();
+        req.socket.destroy();
+    });
+}
---
+++ readme.markdown
@@ -0,0 +1,7 @@
+# mountie
+
+Compose many web server processes into cohesive applications by mounting web
+services registered with [seaport](https://github.com/substack/seaport) at
+mount paths.
+
+# 
